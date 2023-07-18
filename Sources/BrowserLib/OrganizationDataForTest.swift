96e9a6b664f2a60fcf6a58bd12d2da04adf17f83
EREN <erteneren93@gmail.com>
2023-07-18 20:52:06 +0100

    main part finished.

---
+++ Sources/BrowserLib/OrganizationDataForTest.swift
@@ -0,0 +1,11 @@
+//
+//  OrganizationDataForTest.swift
+//  
+//
+//  Created by Eren Erten on 18.07.2023.
+//
+import Foundation
+
+struct OrganizationDataForTest:Decodable{
+    var login:String
+}
